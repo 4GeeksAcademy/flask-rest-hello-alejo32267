e8597a982fc61d84e27f0e54b462d873775e16dc
Alejandro Lopez <123325333+Alejo32267@users.noreply.github.com>
2025-08-08 00:03:19 +0000

    Finalizado

--- src/models.py
+++ src/models.py
@@ -1,19 +1,63 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
 class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    __tablename__ = 'user'
 
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(120), unique=True, nullable=False)
+    first_name = db.Column(db.String(120), nullable=False)
+    last_name = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    is_active = db.Column(db.Boolean, nullable=False)
+
+    comments = db.relationship('Comments', backref='author', lazy=True)
+    posts = db.relationship('Post', backref='user', lazy=True)
+    following = db.relationship('Followers', foreign_keys='Followers.user_from_id', backref='user_from', lazy=True)
+    followers = db.relationship('Followers', foreign_keys='Followers.user_to_id', backref='user_to', lazy=True)
 
     def serialize(self):
         return {
             "id": self.id,
+            "user_name": self.user_name,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "first_name": self.first_name,
+            "last_name": self.last_name,
         }
+
+class Post(db.Model):  
+    __tablename__ = 'post'
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(1000), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    comments = db.relationship('Comments', backref='post', lazy=True)
+    media = db.relationship('Media', backref='post', lazy=True)
+
+class Comments(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comments_text = db.Column(db.String(500), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+class Media(db.Model): 
+    __tablename__ = 'media'
+
+    id = db.Column(db.Integer, primary_key=True)
+    media_type = db.Column(db.String(50), nullable=False)  # image, video, etc.
+    url = db.Column(db.String(255), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+
+class Followers(db.Model):
+    __tablename__ = 'followers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_from_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_to_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
